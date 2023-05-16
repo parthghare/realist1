942dd4bd9e17eafc3a87365fbb2779b6e83dd870
parthghare <ghareparth@gmail.com>
2023-05-16 17:02:47 +0530

    done

---
+++ server/controllers/auth.js
@@ -0,0 +1,93 @@
+import * as config from "../config.js"
+import jwt from 'jsonwebtoken'
+import { emailTemplate } from "../helpers/email.js";
+import  {hashPassword, comparePassword} from "../helpers/auth.js ";
+import User from "../models/user.js";
+import {nanoid} from "nanoid";
+
+export const welcome = (req, res) => {
+    res.json({
+        data: "hello from node hey parth.h.",
+    });
+
+};
+
+export const preRegister = async (req, res) => {
+    //create jwt with email and password then email as clickable link
+    //only when user click on that email link , registeration completes
+    try{
+        // console.log(req.body);
+        const {email, password } = req.body;
+        const token = jwt.sign({email, password}, config.JWT_SECRET, {
+            expiresIn: "1h",
+        });
+
+ 
+        config.AWSSES.sendEmail( emailTemplate(email,
+             `
+        <p>Please click to link below to activate your account.</p>
+        <a href="${config.CLIENT_URL}/auth/account/activate/${token}">Activate my account</a>
+        `,
+        
+        config.REPLY_TO,
+        'Activate your account'
+         ),
+
+          (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json({ ok: false});
+             } else{
+                console.log(data);
+                return res.json({ ok: true});
+
+             } 
+        });
+
+    } catch (err){
+        console.log(err);
+        return res.json({ error: "Somthing weint wrong, try again!!!"});
+    }
+};
+
+export const register = async (req, res) => {
+    try{
+        // console.log(req.body);
+        const {email, password } = jwt.verify(req.body.token, config.JWT_SECRET);
+
+        const hashedPassword = await hashPassword(password);
+
+        const user = await new User({
+            username: nanoid(6),
+            email,
+            password: hashedPassword,
+        }).save();
+
+        const token = jwt.sign({_id: user._id}, config.JWT_SECRET, {
+            expiresIn:"1h",
+        });
+
+        const refreshToken = jwt.sign({_id: user._id}, config.JWT_SECRET, {
+            expiresIn:"7d",
+        });
+        user.password = undefined;
+        user.resetCode = undefined;
+
+        return res.json({
+            token, 
+            refreshToken,
+            user,
+        });
+
+
+
+    }catch (err) {
+        console.log(err);
+        return res.json({ error: "Somthing weint wrong, try again!!!"});                                                                 
+    }
+};
+
+
+
+
+
